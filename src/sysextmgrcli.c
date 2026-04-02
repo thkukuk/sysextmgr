d8a72b8d40cf99b15fdd51b34f9dc6d96e17e7bc
Stefan Schubert <schubi@suse.de>
2026-04-02 15:58:13 +0200

    -a option for image-list

--- src/sysextmgrcli.c
+++ src/sysextmgrcli.c
@@ -58,6 +58,7 @@ usage(int retval)
   fputs("list - list all images and if they are compatible\n", output);
   fputs("Options for merge-json:\n", output);
   fputs("  -u, --url URL         Remote directory with sysext images\n", output);
+  fputs("  -a, --all             List images with incompatible architecture too\n", output);
   fputs("  -v, --verbose         Verbose output\n", output);
   fputs("\n", output);
 
