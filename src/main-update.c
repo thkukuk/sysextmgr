49229a8b88dd3169722640faadd39574fd59e1f3
Stefan Schubert <schubi@suse.de>
2026-04-16 17:58:25 +0200

    cleanup

--- src/main-update.c
+++ src/main-update.c
@@ -4,10 +4,12 @@
 
 #include <getopt.h>
 #include <stdbool.h>
+#include <libsmartcols/libsmartcols.h>
 
 #include "basics.h"
 #include "sysextmgr.h"
 #include "varlink-client.h"
+#include "pager.h"
 
 static bool arg_verbose = false;
 static bool arg_quiet = false;
@@ -56,6 +58,8 @@ varlink_update (const char *url, const char *prefix)
   sd_json_variant *result;
   const char *error_id = NULL;
   int r;
+  struct libscols_table *table = NULL;
+  struct libscols_line *line = NULL;
 
   r = connect_to_sysextmgrd(&link, _VARLINK_SYSEXTMGR_SOCKET);
   if (r < 0)
@@ -133,9 +137,21 @@ varlink_update (const char *url, const char *prefix)
       return -EINVAL;
     }
 
-  /* XXX Use table_print_with_pager */
   if (!arg_quiet && sd_json_variant_elements(p.contents_json) > 0)
-    printf ("Old image -> New image\n");
+    {
+      /* Initialize the table */
+      table = scols_new_table();
+      if (!table)
+        {
+	  fprintf(stderr, "Failed to allocate table\n");
+	  return -EIO;
+        }
+
+      // Define Column Headers
+      scols_table_new_column(table, "Old image", 0, 0);
+      scols_table_new_column(table, "New image", 0, 0);
+      scols_table_set_column_separator(table, " -> ");
+    }
 
   for (size_t i = 0; i < sd_json_variant_elements(p.contents_json); i++)
     {
@@ -165,7 +181,19 @@ varlink_update (const char *url, const char *prefix)
         }
 
       if (!arg_quiet)
-	printf ("%s -> %s\n", e.old_name, strna(e.new_name));
+	{
+          line = scols_table_new_line(table, NULL);
+          scols_line_sprintf(line, 0, "%s", e.old_name);
+          scols_line_sprintf(line, 1, "%s", strna(e.new_name));
+	}
+    }
+
+  if (table)
+    {
+      /* Setup Pager and Print */
+      pager(table,"");
+
+      scols_unref_table(table);
     }
 
   return 0;
